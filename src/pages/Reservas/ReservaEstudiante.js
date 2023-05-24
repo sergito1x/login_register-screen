0f9872462766269bade2b155672b1d696227b883
O26407 <O26407@lab.aca>
2023-05-24 09:39:32 -0500

    Docentes Disponibles

--- src/pages/Reservas/ReservaEstudiante.js
+++ src/pages/Reservas/ReservaEstudiante.js
@@ -1,20 +1,57 @@
-import React,{ useState } from "react";
-import Title from "./components/Title/Title.jsx";
+import React, { useState } from "react";
+import Title from "../Login/components/Title/Title.jsx";
 import Input from "./components/Input/Input.jsx";
 import SwitchButton from "./components/SwitchButton/SwitchButton.jsx";
+import DocentesDisponibles from "./components/DocentesDisponibles/DocentesDisponibles.jsx";
 
 import './ReservaEstudiante.css';
 
+const handleInputSearch = () => {
+  // Lógica para manejar la búsqueda del input
+}
+
 const ReservaEstudiante = () => {
-    return (
+  const [listaDocentes, setListaDocentes] = useState([
+    {
+        name: 'Pepe',
+        university: 'Universidad de Lima',
+        course: 'Ingeniería de Sistemas'
+    },
+    {
+        name: 'Steven',
+        university: 'Universidad de Lima',
+        course: 'Programación Web'
+    },
+    {
+        name: 'Sergio',
+        university: 'Universidad de Ibagué',
+        course: 'Programación Móvil'
+    }
+  ]);
+
+  const MapDocentes = (listaDocentes) => {
+    return listaDocentes.map((docente) => {
+      return (
         <div>
-            <Title className='title-container' text='Reserva de Cita'/>
-            <hr className='divider'/>
-            <div>
-                <Input/><SwitchButton/>
-            </div>
+            <DocentesDisponibles docente={docente} className="docentes-container"/>
         </div>
-    )
+      );
+    });
+  };
+
+  return (
+    <div>
+      <Title className='title-container' text='Reserva de Cita'/>
+      <hr className='divider'/>
+      <div className='search-container'>
+        <Input handleChange={handleInputSearch} className="input-search" />
+        <h1></h1>
+        <SwitchButton className="switch" />
+      </div>
+      <br />
+      {MapDocentes(listaDocentes)}
+    </div>
+  );
 }
 
 export default ReservaEstudiante;
---
+++ src/pages/Reservas/components/DocentesDisponibles/DocentesDisponibles.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DocentesDisponibles.css'
+
+const DocentesDisponibles = ({docente}) => {
+    return (
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{docente.name}</h5>
+                <p class="card-text">{docente.university} - {docente.course}</p>
+                <a href="#" class="btn btn-primary"> &gt; </a>
+            </div>
+        </div>
+    )
+}
+
+export default DocentesDisponibles
