97b14afae50c0dc82bdf81d842204d262a34860e
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-05-25 22:56:34 -0500

    Borré Mi Perfil porque estaba en HTML, en React está en otra rama

--- src/pages/MiPerfil/JS/funcionesBarra.js
+++
@@ -1,9 +0,0 @@
-function verPagina(nombrePagina) {
-    var paginas = document.getElementsByClassName('pagina');
-    for (var i = 0; i < paginas.length; i++) {
-        paginas[i].style.display = 'none';
-    }
-
-    var paginaSeleccionada = document.getElementById(nombrePagina);
-    paginaSeleccionada.style.display = 'block';
-}
