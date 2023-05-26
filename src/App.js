bf0572515c5b0e43ff260866cd6c7c5fea78c5b7
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-05-25 23:39:07 -0500

    Finish

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import ReservaEstudiante from './pages/Reservas/ReservaEstudiante';
 import MisCalificaciones from './pages/MisCalificaciones/MisCalificaciones';
-import Horarios from './pages/Horario/Horarios'
+import Horarios from './pages/Horario/Horarios';
 import MiPerfil from './pages/MiPerfil/MiPerfil';
 
 function App() {
-  return (
-    <div className="App">
-      
-     
-      <Register />
-      <Login />
-      <hr/>
-      <ReservaEstudiante />
-      <hr/>
-      <MisCalificaciones />
-      <hr/>
-      <Horarios></Horarios>
-    </div>
-  );
+  const [pantallaValeria, setPantallaValeria] = useState(false);
+
+  const cambiarPantalla = () => {
+    setPantallaValeria(!pantallaValeria);
+  };
+
+  if (pantallaValeria) {
+    return (
+      <div>
+        <MiPerfil />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <button onClick={cambiarPantalla}>Cambiar</button>
+        <Register />
+        <hr />
+        <Login />
+        <hr />
+        <ReservaEstudiante />
+        <hr />
+        <MisCalificaciones />
+        <hr />
+        <Horarios></Horarios>
+        <button onClick={cambiarPantalla}>Cambiar</button>
+      </div>
+    );
+  }
 }
 
 export default App;
